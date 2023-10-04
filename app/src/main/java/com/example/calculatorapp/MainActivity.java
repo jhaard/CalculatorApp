599acc38fe1d5a052ba24dbe72e59b087228835e
Juhee Kang <juhee.kang@infotiv.se>
2023-10-04 15:05:15 +0200

    wip add function add, sub, mul, div

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -1,15 +1,113 @@
 package com.example.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText firstNumber,secondNumber;
+    Button additionButton, substractButton, multipleButton, divisionButton, squareRootButton;
+    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firstNumber = findViewById(R.id.firstNumber);
+        //secondNumber = findViewById(R.id.secondNumber);
+        additionButton = findViewById(R.id.additionButton);
+        substractButton = findViewById(R.id.subtractionButton);
+        multipleButton = findViewById(R.id.multiplicationButton);
+        divisionButton = findViewById(R.id.divisionButton);
+        squareRootButton = findViewById(R.id.squareRootButton);
+        resultText = findViewById(R.id.textViewResult);
+
+        // Addition
+        additionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((firstNumber.getText().length()>0) && (secondNumber.getText().length()>0))
+                {
+                    double firstN = Double.parseDouble(firstNumber.getText().toString());
+                    double secondN = Double.parseDouble(secondNumber.getText().toString());
+                    double result = firstN + secondN;
+                    resultText.setText(Double.toString(result));
+                }
+            }
+        });
+
+        //Subtraction
+        substractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((firstNumber.getText().length()>0) && (secondNumber.getText().length()>0))
+                {
+                    double firstN = Double.parseDouble(firstNumber.getText().toString());
+                    double secondN = Double.parseDouble(secondNumber.getText().toString());
+                    double result = firstN - secondN;
+                    resultText.setText(Double.toString(result));
+                }
+            }
+        });
+
+        // Multiplication
+        multipleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((firstNumber.getText().length()>0) && (secondNumber.getText().length()>0))
+                {
+                    double firstN = Double.parseDouble(firstNumber.getText().toString());
+                    double secondN = Double.parseDouble(secondNumber.getText().toString());
+                    double result = firstN * secondN;
+                    resultText.setText(Double.toString(result));
+                }
+            }
+        });
+
+        // Division (need to upgrade!)
+        divisionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((firstNumber.getText().length()>0) && (secondNumber.getText().length()>0))
+                {
+                    double firstN = Double.parseDouble(firstNumber.getText().toString());
+                    double secondN = Double.parseDouble(secondNumber.getText().toString());
+                    double result = firstN / secondN;
+                    resultText.setText(Double.toString(result));
+                }
+                else{
+                    if((firstNumber.getText().length()<0) || (secondNumber.getText().length()<0)) {
+                        Toast toast= Toast.makeText(MainActivity.this,
+                                "Enter The Required Numbers",Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                    Toast toast= Toast.makeText(MainActivity.this,
+                            "Enter The Required Numbers",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+        });
+
+        // Square root
+        squareRootButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((firstNumber.getText().length()>0) || (secondNumber.getText().length()>0))
+                {
+                    double firstN = Double.parseDouble(firstNumber.getText().toString());
+                    //double secondN = Double.parseDouble(secondNumber.getText().toString());
+                    double result = Math.sqrt(firstN);
+                    resultText.setText(Double.toString(result));
+                }
+            }
+        });
+
+
     }
 }
 
